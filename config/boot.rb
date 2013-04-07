1370159933836f65c5a7f4563db0392b9e6f9f2b
Erik Guzman <me.erik@me.com>
2013-04-07 02:05:52 -0700

    first commit

---
+++ config/boot.rb
@@ -0,0 +1,10 @@
+if ENV['RAILS_ENV'] == 'production'
+  Gem.clear_paths
+end
+require 'rubygems'
+
+# Set up gems listed in the Gemfile.
+if File.exist?(File.expand_path('../../Gemfile', __FILE__))
+  require 'bundler'
+  Bundler.setup
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnimeErikkatCom::Application.config.secret_token = '4eb201f59c119b0dd3092f10f104f141d22e8ba8d42277c8a8c23f27c8d9e26f43c7b61d2bc82273976a598f7376a3038712d219d2b8b8f99fdc63f09c3c974c'
