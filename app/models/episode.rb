18057ba65c3790aa7cf835e2ef48032d627a04e8
Erik Guzman <me.erik@me.com>
2013-04-20 10:29:38 -0700

    improved episode query to work for sql

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -5,5 +5,5 @@ class Episode < ActiveRecord::Base
   validates :name, :number, :url, :anime_show_id, :presence => true
   validates :number, :uniqueness => { :scope => :anime_show_id }
 
-  scope :unseen_episodes, lambda { |user| where("episodes.id NOT IN (?)", user.user_seen_episodes.select(:episode_id).collect(&:episode_id)).order('created_at DESC') }
+  scope :unseen_episodes, lambda { |user| where("episodes.id NOT IN (?)", user.user_seen_episodes.select(:episode_id).map(&:episode_id)) }
 end
