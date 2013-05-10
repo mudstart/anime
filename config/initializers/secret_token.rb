9ba287b58a0b6fdd740a843e259b5613cbf7276a
uhoherk <eguzmail@gmail.com>
2013-05-10 14:10:40 -0700

    upgraded code to hide secret token for production, moved secret token for production into figaro application.yml file that is gitignore

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,5 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AnimeErikkatCom::Application.config.secret_token = '4eb201f59c119b0dd3092f10f104f141d22e8ba8d42277c8a8c23f27c8d9e26f43c7b61d2bc82273976a598f7376a3038712d219d2b8b8f99fdc63f09c3c974c'
+# rake secret to make a new token
+AnimeErikkatCom::Application.config.secret_token = ENV['SECRET_TOKEN'] || '4eb201f59c119b0dd3092f10f104f141d22e8ba8d42277c8a8c23f27c8d9e26f43c7b61d2bc82273976a598f7376a3038712d219d2b8b8f99fdc63f09c3c974c'
