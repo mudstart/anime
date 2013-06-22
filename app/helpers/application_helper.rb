c84acf2cb5ab3569dddafde00d448a049d40d427
uhoherk <eguzmail@gmail.com>
2013-06-22 13:18:13 -0700

    intro home page improved with better styling, font and background

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,16 @@ module ApplicationHelper
       raw '<span class="label label-info">NEW</span>'
     end
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
