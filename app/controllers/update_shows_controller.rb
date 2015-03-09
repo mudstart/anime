bab5b2367d7664ee35839c4136e616a2eeeb0c0c
Erik Guzman <me.erik@me.com>
2015-03-08 17:14:51 -0700

    Formatting adjustments

--- app/controllers/update_shows_controller.rb
+++ app/controllers/update_shows_controller.rb
@@ -3,9 +3,9 @@ class UpdateShowsController < ApplicationController
 
   def update
     NarutoWireCrawler.delay.update_show(@anime_show)
-     respond_to do |format|
-       format.html { redirect_to :back, :notice => 'Show is updating' }
-     end
+    respond_to do |format|
+      format.html { redirect_to :back, :notice => 'Show is updating' }
+    end
   end
 
 end
